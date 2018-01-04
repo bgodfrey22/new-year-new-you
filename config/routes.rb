71376788b63984494afe7dd27ab10b75ecca2a4c
Brandy Godfrey <bgodfrey22@gmail.com>
2018-01-03 21:24:16 -0500

    Home page carousel

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :exercises
   devise_for :users
 
-  root 'pages#index'
+  get 'about' => 'pages#about'
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
