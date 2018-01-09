7bc36ab43837fe6ba295fa195de746790655eb97
Brandy Godfrey <bgodfrey22@gmail.com>
2018-01-09 11:42:15 -0500

    Styling changes to views

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
+  get 'show_user' => 'pages#show_user'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
