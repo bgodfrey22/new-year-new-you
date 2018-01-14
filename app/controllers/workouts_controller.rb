78daebf8df4ca71968e90d5e7ca582386a0e20ba
Brandy Godfrey <bgodfrey22@gmail.com>
2018-01-14 17:35:14 -0500

    update edit view and controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -11,7 +11,7 @@ class WorkoutsController < ApplicationController
 
   def new
     @workout = Workout.new
-    @workout.exercises.build
+    @exercises_workout = ExercisesWorkout.new
   end
 
   def edit
@@ -26,23 +26,21 @@ class WorkoutsController < ApplicationController
   end
 
   def create
-    @workout = Workout.new(workout_params)
+    @workout = Workout.create(workout_params)
     if @workout.save
-      redirect_to workouts_path(id: params[:id])
+      redirect_to edit_workout_path(id: params[:id])
     else
       render :new
     end
   end
 
   def update
-    respond_to do |format|
-     if @workout.update(workout_params)
-       format.html { redirect_to @workout, notice: 'Exercise was successfully updated.' }
-       format.json { render :show, status: :ok, location: @workout }
-     else
-       format.html { render :edit }
-       format.json { render json: @workout.errors, status: :unprocessable_entity }
-     end
+    @workout = Workout.find_by(id: params[:id])
+    @exercises_workout = ExercisesWorkout.create(exercises_workout_params)
+    if @workout.update_attributes(workout_params)
+      redirect_to workouts_path(id: params[:id])
+    else
+      render :edit
     end
   end
 
@@ -61,7 +59,7 @@ class WorkoutsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def workout_params
-    params.require(:workout).permit(:label, exercises_attributes: [:name, :muscle_group])
+    params.require(:workout).permit(:label)
   end
   def exercises_workout_params
     params.require(:exercises_workout).permit(:exercise_id, :workout_id, :destroy)
