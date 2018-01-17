c06a5eb30551ec16f53daf31eca33de5d98be14c
Brandy Godfrey <bgodfrey22@gmail.com>
2018-01-16 20:28:04 -0500

    ajax for exercises views

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,16 +1,8 @@
 class ExercisesController < ApplicationController
+  before_action :all_exercises, only: [:index, :create, :update, :destroy]
   before_action :set_exercise, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:show]
   
-  # GET /exercises
-  def index
-    @exercises = Exercise.all
-  end
-
-  # GET /exercises/1
-  def show
-  end
-
   # GET /exercises/new
   def new
     @exercise = Exercise.new
@@ -22,42 +14,23 @@ class ExercisesController < ApplicationController
 
   # POST /exercises
   def create
-    @exercise = Exercise.new(exercise_params)
-
-    respond_to do |format|
-      if @exercise.save
-        format.html { redirect_to @exercise, notice: 'Exercise was successfully created.' }
-        format.json { render :show, status: :created, location: @exercise }
-      else
-        format.html { render :new }
-        format.json { render json: @exercise.errors, status: :unprocessable_entity }
-      end
-    end
+    @exercise = Exercise.create(exercise_params)
   end
 
   # PATCH/PUT /exercises/1
   def update
-    respond_to do |format|
-      if @exercise.update(exercise_params)
-        format.html { redirect_to @exercise, notice: 'Exercise was successfully updated.' }
-        format.json { render :show, status: :ok, location: @exercise }
-      else
-        format.html { render :edit }
-        format.json { render json: @exercise.errors, status: :unprocessable_entity }
-      end
-    end
+    @exercise.update(exercise_params)
   end
 
   # DELETE /exercises/1
   def destroy
     @exercise.destroy
-    respond_to do |format|
-      format.html { redirect_to exercises_url, notice: 'Exercise was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
+    def all_exercises
+      @exercises = Exercise.all
+    end 
     # Use callbacks to share common setup or constraints between actions.
     def set_exercise
       @exercise = Exercise.find(params[:id])
