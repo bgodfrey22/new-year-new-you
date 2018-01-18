e0aa689309cdccaad7ea7ddb8b0ccf9184416d59
Brandy Godfrey <bgodfrey22@gmail.com>
2018-01-18 09:14:41 -0500

    update show user and workout index with partial

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
   get 'show_user' => 'pages#show_user'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
